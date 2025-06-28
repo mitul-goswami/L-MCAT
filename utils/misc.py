3a721911b6dbd1320c6bd03f6316cc6b90a99c51
Mitul Goswami <126082561+mitul-goswami@users.noreply.github.com>
2025-06-29 01:00:05 +0530

    Update misc.py

--- utils/misc.py
+++ utils/misc.py
@@ -1,1 +1,19 @@
+import torch
+import random
+import numpy as np
+import yaml
 
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+def save_checkpoint(state, filename):
+    torch.save(state, filename)
+    
+def load_config(config_path):
+    with open(config_path) as f:
+        return yaml.safe_load(f)
